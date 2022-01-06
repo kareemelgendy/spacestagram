1cd438520db908f12c8ae9de9ccb2086bab60724
Kareem <kxreem@gmail.com>
2022-01-05 20:03:39 -0500

    Improved accessibility and SEO scores

---
+++ src/pages/_document.tsx
@@ -0,0 +1,29 @@
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta
+            name="description"
+            content="Spacetagram. An image-sharing application using NASAs picture of the day API"
+          />
+          <meta name="theme-color" content="#efefef" />
+          <link rel="preconnect" href="https://apod.nasa.gov/" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
