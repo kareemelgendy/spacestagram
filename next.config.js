4f0bc27e6bb7bb034f3551a695b2155a3cf8bc2c
Kareem <kxreem@gmail.com>
2022-01-05 20:44:49 -0500

    Added vimeo hostname to config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['apod.nasa.gov', 'img.youtube.com']
+    domains: ['apod.nasa.gov', 'img.youtube.com', 'i.vimeocdn.com']
   }
 }
